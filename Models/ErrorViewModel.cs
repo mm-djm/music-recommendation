6d6de5e85c48e8b61fb9bffd97aad2f6e51af302
mira.dai <mira.dai@ucloud.cn>
2022-08-17 19:42:11 +0800

    first version

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace music_recommdation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
